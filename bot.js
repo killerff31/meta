1da7d02c7bc44ce71b3d5dacbf52b903cac278e4
killerff31 <farhankiller31@gmail.com>
2024-09-28 20:12:34 +0530

    Update and rename index.js to bot.js

---
+++ bot.js
@@ -0,0 +1,31 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = process.env.TELEGRAM_BOT_TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, 'Enter a Google Drive file link:');
+});
+
+bot.on('message', (msg) => {
+  if (msg.text) {
+    const link = msg.text;
+    const fileId = extractFileId(link);
+
+    if (fileId) {
+      const downloadLink = `https://www.googleapis.com/drive/v3/files/${fileId}?alt=media&key=AIzaSyB2Gi6A21kfBvBDs3MRfF5yKrp-nxmRbLQ`;
+      bot.sendMessage(msg.chat.id, downloadLink, {
+        caption: 'Direct Download Link',
+      });
+    } else {
+      bot.sendMessage(msg.chat.id, 'Invalid Google Drive file link format.');
+    }
+  }
+});
+
+function extractFileId(link) {
+  const regex = /\/file\/d\/([a-zA-Z0-9_-]+)/;
+  const match = link.match(regex);
+  return match && match[1];
+}
+
